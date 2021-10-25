424e07fdb792c9c8bee14bdb1ab7bcb5c81c0367
Santiago <S.tmarco@alumnos.upm.es>
2021-10-25 10:58:54 +0200

    test addFirst

--- src/test/java/com/practica/cajanegra/PruebaSanti.java
+++ src/test/java/com/practica/cajanegra/PruebaSanti.java
@@ -6,6 +6,10 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.runner.notification.RunListener.ThreadSafe;
 
+import org.junit.jupiter.params.ParameterizedTest;
+
+import org.junit.jupiter.params.provider.ValueSource;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class PruebaSanti {
@@ -22,12 +26,11 @@ public class PruebaSanti {
 	    public void setUp() {
 		    this.miLista = new SingleLinkedListImpl<String>("A","B","C");
 	    }
-
-        @Test
-
+        
         @ParameterizedTest(name="Add First {0} in list")
         @ValueSource(strings= {"@", "A", "B", "M", "Y", "Z", "["})
 
+        @Test
 	    public void addFirst(String s) {
 		    this.miLista.addFirst(s);
 		    assertEquals("[" + s + ", A, B, C]", this.miLista.toString());
