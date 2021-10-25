7a4479d3225025556943d890ab4f7e6411f186a6
Santiago <S.tmarco@alumnos.upm.es>
2021-10-25 09:44:29 +0200

    prueba.java en cajanegra

--- src/test/java/com/practica/cajanegra/Prueba.java
+++ src/test/java/com/practica/cajanegra/Prueba.java
@@ -1,9 +1,10 @@
+package com.practica.cajanegra;
 import javax.sound.sampled.SourceDataLine;
 import javax.swing.plaf.basic.BasicInternalFrameTitlePane.SystemMenuBar;
 
 public class Prueba {
-    private static Type name() {
-        
+    private static void main(String arg[]) {
+        int prueba=0;
     }
     
 }
